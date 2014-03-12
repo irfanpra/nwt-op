95302418a9e6840e527ab71ca12b9d932e88350e
Ahmetović Emir <a.emir.91@gmail.com>
2014-03-12 21:26:39 +0100

    UserLogin scafold

---
+++ app/views/user_logins/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user_login, :id, :username, :password, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 NWTMarketinghub::Application.routes.draw do
+  resources :user_logins
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
