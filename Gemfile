463cf31aee66a5311a93ea01b57e9bc128785bec
Irfan Prazina <irfanpra@gmail.com>
2014-03-12 23:37:51 +0100

    route i gem popravljeno

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'mysql2'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
-
+gem 'wash_out'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 NWTMarketinghub::Application.routes.draw do
+  
   get "ads/index"
   resources :user_logins
-
+  wash_out :rumbas
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
