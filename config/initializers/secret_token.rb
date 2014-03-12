9b1f7e050056dbc6465b49d40cbb062160c38f97
Ahmetović Emir <a.emir.91@gmail.com>
2014-03-12 20:23:56 +0100

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NWTMarketinghub::Application.config.secret_key_base = 'dc2aefefd036db2d4857233bcdef6a62db59b8f5782386f746710c074d98303e40ce83bcc96e000233977957e05a7c826cbd96b2c202fa31fb3a21f7d28a1c02'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NWTMarketinghub::Application.config.session_store :cookie_store, key: '_NWT-marketingHub_session'
