57eb2b0e0283fc8ec11d6b036a95b1a2b3584e3f
Ahmetović Emir <a.emir.91@gmail.com>
2014-03-12 23:57:15 +0100

    Popravljen review model

--- db/migrate/20140312223909_create_reviews.rb
+++ db/migrate/20140312223909_create_reviews.rb
@@ -1,7 +1,7 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-      t.int :rating
+      t.integer :rating
       t.text :content
 
       t.belongs_to :ad
