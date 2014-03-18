017171e0d3bf403053ccdeefcbbc45b1cddc04bb
Ahmetović Emir <a.emir.91@gmail.com>
2014-03-18 20:08:12 +0100

    izbrisani zarezi

--- db/migrate/20140318185739_create_ad_offers.rb
+++ db/migrate/20140318185739_create_ad_offers.rb
@@ -1,15 +1,15 @@
 class CreateAdOffers < ActiveRecord::Migration
   def change
     create_table :ad_offers do |t|
-      t.string, :title
-      t.string, :subtitle
-      t.text, :description
-      t.decimal, :price
-      t.string, :unit
-      t.datetime, :date_start
-      t.datetime, :date_end
-      t.boolean, :is_hidden
-      t.int, :views_count
+      t.string :title
+      t.string :subtitle
+      t.text :description
+      t.decimal :price
+      t.string :unit
+      t.datetime :date_start
+      t.datetime :date_end
+      t.boolean :is_hidden
+      t.int :views_count
       t.integer :max_duration
 
       t.timestamps
